68ac6d7f197e1af3296aad318c0f731c6040231d
Proudust <proudust@gmail.com>
2023-07-16 16:00:53 +0900

    :arrow_up: Bump deno.land/std from 0.182.0 to 0.190.0 and more

--- _build.ts
+++ _build.ts
@@ -1,6 +1,6 @@
 import gasPlugin from "https://esm.sh/esbuild-gas-plugin@0.6.0/mod.ts";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
-import { parse } from "https://deno.land/std@0.182.0/flags/mod.ts";
+import { parse } from "https://deno.land/std@0.190.0/flags/mod.ts";
 import { build } from "https://deno.land/x/esbuild@v0.17.19/mod.js";
 import $ from "https://deno.land/x/dax@0.33.0/mod.ts";
 
--- main.test.ts
+++ main.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 
 Deno.test("Your test here", () => {
   assertEquals(1 + 1, 2);
